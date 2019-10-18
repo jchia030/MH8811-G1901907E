bc138561afd46a3da0f3d57fdf8def0a1f545ff4
jchia030 <55873474+jchia030@users.noreply.github.com>
2019-10-19 01:59:00 +0800

    Update tictactoe.py

--- 06/tictactoe.py
+++ 06/tictactoe.py
@@ -1,12 +1,13 @@
 def TicTacDraw(board):
-    tictacmap={0:'O',1:'X',2:' '}
+    tictacmap={0:' O ',1:' X ',2:'   '}
     k=0
     for i in board:
         k+=1
         string=''
         for j in i:
             string+=tictacmap[j]
-        row='|'.join(string)
+            string+='|'
+        row=string[:-1]
         print(row)
         if k<len(board):
-            print('--'*len(board))
+            print('----'*len(board))
