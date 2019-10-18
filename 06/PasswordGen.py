303f07ad0378b19bfa264a43d1fdb2c18005d733
jchia030 <55873474+jchia030@users.noreply.github.com>
2019-10-19 02:23:31 +0800

    Update PasswordGen.py

--- 06/PasswordGen.py
+++ 06/PasswordGen.py
@@ -1,8 +1,7 @@
-import random
-import string
-import re
-
 def genPassword(n):
+    import random
+    import string
+    import re
     i=0
     while i<4:    
         password=''.join([random.choice(string.ascii_letters + string.digits + string.punctuation) for n in range(n)])
@@ -15,6 +14,6 @@ def genPassword(n):
             i+=1
         if re.search(r'[@_!#$%^&*()<>?/\|}{~:]',password):
             i+=1
-    print(password)
+    return password
     
-genPassword(12)
+print(genPassword(12))
