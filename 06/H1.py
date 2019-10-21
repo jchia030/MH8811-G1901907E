ecabddbf7b0cafc30abf452aa51eb1fbe9b520e8
jchia030 <55873474+jchia030@users.noreply.github.com>
2019-10-21 22:34:33 +0800

    Update H1.py

--- 06/H1.py
+++ 06/H1.py
@@ -1,5 +1,7 @@
 from genPassword import Passwordgen
 
 length=input('How long is your password?')
+while length<4:
+    length=input('Pls key in at least 4. How long is your password?')
 print(Passwordgen(length))
     
